7aa595bc0fded0757df7d0de8c226fddc02afc27
FlyFastCode <gan.web3@gmail.com>
2026-04-09 16:30:57 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>ShipDesign33 React App</h1>
+      <h1>ShipDesign33 Ship Ship</h1>
       <p>欢迎使用 React + TypeScript + Vite 基础框架</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
